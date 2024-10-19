2411678e6f85d7294c526f6334a93503ea8dc2c9
callumanderson <callum.g.anderson@gmail.com>
2024-10-19 14:17:46 -0400

    init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Perceptron - Machine Learning, Assignment 5 - Part 2.2
+ *
+ * Callum Anderson
+ * T00708915
+ * October 20, 2024
+ *
+ * */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
